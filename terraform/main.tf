e5ac3a050069d08166ea9e287ce9b5bdaed97ec3
Politess <david18tran@outlook.com>
2024-10-17 22:04:39 +0100

    modified var

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,16 +9,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 module "backend" {
   source        = "./modules/remote_backend"
-  iam_user_name = var.iam_user_name
-  bucket_name   = var.bucket_name
-  table_name    = var.table_name
-}
-
-output "iam_user_name" {
-  value = module.backend.iam_user_arn
+  iam_user_name = "cloudchallenge"
+  bucket_name   = "bucket_test"
+  table_name    = "table_test"
 }
